f35e11b65728758ce5da05674399d4c484320631
Harold <telemac.dev@gmail.com>
2021-09-26 21:37:53 -0400

    Configurado Travis CI
    close #12

--- README.md
+++ README.md
@@ -5,6 +5,8 @@ Nesse curso é ensinado como contribuir com projetos de código aberto
 
 Link para o curso [Python Pro](https://www.python.pro.br)
 
+[![Build Status](https://app.travis-ci.com/telemac-dev/libpythonpro.svg?branch=main)](https://app.travis-ci.com/telemac-dev/libpythonpro)
+
 Supotada versão 3 de python
 
 Tópicos a serem abordados:
--- libpythonpro/github_api.py
+++ libpythonpro/github_api.py
@@ -3,12 +3,9 @@ import requests
 
 def buscar_avatar(usuario):
     """
-    Buscar o avatar de um usuário no Github
-
-    :param usuario: str com o nome de usuário no Github
-    :return: str o link do avatar
+    Buscar o avatar de um usuário no Github     :param usuario: str com o nome de usuário no Github     :return: str o link do avatar
     """
-    url=f'https://api.github.com/users/{usuario}'
+    url = f'https://api.github.com/users/{usuario}'
     resp = requests.get(url)
     return resp.json()['avatar_url']
 
